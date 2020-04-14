a88c286b8ad345c30b15c94e9e88d4b994c33757
github username <github email address>
2020-04-14 11:52:20 -0500

    nav bar login and signup links

--- cookbookhero-frontend/src/HomePage.js
+++ cookbookhero-frontend/src/HomePage.js
@@ -8,7 +8,7 @@ class HomePage extends Component {
       <div>
         <HomeNavBar />
         <div className='centered' >
-          <h1>Cook Book Hero Title</h1>
+          <h1 className='site-title'>Cook Book Hero Title</h1>
           <img
             src="https://via.placeholder.com/550x375"
             alt="Cook Book Hero Recipe Organizer"
--- cookbookhero-frontend/src/components/HomeNavBar.js
+++ cookbookhero-frontend/src/components/HomeNavBar.js
@@ -6,9 +6,9 @@ class HomeNavBar extends Component {
     return (
       <div>
         <ul style={{listStyleType: 'none', margin: 10, padding: 10}}>
-        <li>Small Logo</li>
-        <li><Link to='/sign-up'>Sign-Up</Link></li>
-        <li><Link to='/login'>Login</Link></li>
+        <li style={{display: 'inline'}}>Small Logo</li>
+        <li style={{display: 'inline', float: 'right', padding: '8px'}}><Link to='/login'>Login</Link></li>
+        <li style={{display: 'inline', float: 'right', padding: '8px'}}><Link to='/sign-up'>Sign-Up</Link></li>
         </ul>
         
         
