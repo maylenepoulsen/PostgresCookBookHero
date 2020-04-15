edc0aebee01fe126259570e39ef6e4bb45e35ba0
lughjw <lughjw@gmail.com>
2020-04-15 16:13:45 -0500

    fixed stuff

--- cookbookhero-frontend/src/components/Tags.js
+++ cookbookhero-frontend/src/components/Tags.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 
 class Tags extends Component {
+  
+  extractTags = () => {
+    const tags = this.props.recipes.map(recipe => recipe.tags).flat(1)
+    // console.log(tags)
+    return tags;
+  }
+
   render() {
-    //console.log("tags", this.props.recipes.tags);
-    if(this.props.recipe) {
+    // console.log("tags", this.props.recipes);
+    if(this.props.recipes) {
     return (
       <div>
         Tags:
-        {/* {this.props.recipes.tags.map((tag, idx) => (
-          <p key={idx}>{tag}</p>
-        ))} */}
+        {this.extractTags().map((tag,idx) => <p key={idx}>{tag}</p>)}
       </div>
     );
       } else return <div></div>
