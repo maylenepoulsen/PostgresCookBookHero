3936182634ce8491fdda3789030aa1bb0aa72af6
lughjw <lughjw@gmail.com>
2020-04-16 12:41:14 -0500

    removed unnecessary recipe data from user_serializer

--- cookbookhero-backend/app/serializers/api/v1/user_serializer.rb
+++ cookbookhero-backend/app/serializers/api/v1/user_serializer.rb
@@ -1,20 +1,3 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-    attributes :id, :name, :username, :recipes
-    def recipes
-        self.object.recipes.map do |r|
-            {
-                id: r.id,
-                name: r.name,
-                history: r.history,
-                description: r.description,
-                ingredients: r.recipe_ingredients.map do |ri|
-                    {
-                        name: ri.name,
-                        quantity: ri.quantity,
-                        unit: ri.unit
-                    }
-                end
-            }
-        end
-    end
+    attributes :id, :name, :username
 end
